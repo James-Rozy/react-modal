5bfde12fca1f26de8e7c6c26923421b2ed9b42e1
James Rozsypal <jimmerxl@gmail.com>
2022-11-18 14:29:08 -0500

    Move css-in-js styling to style.css

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import React, { useState } from 'react';
 import Modal from './components/Modal';
-
-const BUTTON_WRAPPER_STYLES = {
-  position: 'relative',
-  zIndex: 1,
-};
-
-const OTHER_CONTENT_STYLES = {
-  position: 'relative',
-  zIndex: 2,
-  backgroundColor: 'salmon',
-  padding: '10px',
-};
+import './styles/styles.css';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <div style={BUTTON_WRAPPER_STYLES}>
-        <button className='open-modal-btn' onClick={() => setIsOpen(true)}>
-          Open Modal
-        </button>
+    <div className='container'>
+      <header>
+        <h1>React Modal Prototype</h1>
+      </header>
 
-        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-          Here is a modal!
-        </Modal>
-      </div>
+      <div className='content'>
+        <div className='btn-wrapper'>
+          <button className='open-modal-btn' onClick={() => setIsOpen(true)}>
+            Open Modal
+          </button>
 
-      <div className='content' style={OTHER_CONTENT_STYLES}>
-        Content
+          <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+            Here is a modal!
+          </Modal>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
