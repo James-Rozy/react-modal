85d3f8afa219b257e206e7c59e435bb39f518881
James Rozsypal <jimmerxl@gmail.com>
2022-12-06 10:06:38 -0500

    Move content into seperate components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
 import './styles/styles.css';
-import Modal from './components/Modal';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import MainContent from './components/MainContent';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='container'>
-      <Header>
-        <h1>React Modal Prototype</h1>
-      </Header>
-
-      <div className='content'>
-        <div className='btn-wrapper'>
-          <button className='open-modal-btn' onClick={() => setIsOpen(true)}>
-            Open Modal
-          </button>
-
-          <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
-        </div>
-      </div>
+      <Header />
+      <MainContent />
     </div>
   );
 };
